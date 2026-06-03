a20d944f906a6e4a3048394b24010cb8dc97a21f
AveryanAlex <alex@averyan.ru>
2026-06-04 00:26:33 +0300

    Remove dimensional_quantity

--- src/constants.rs
+++ src/constants.rs
@@ -1,12 +1,5 @@
-use dimensional_quantity::si::extended::f64::{
-    constants::MOLAR_GAS_CONSTANT, units_of_measure::molar_entropy::KILOCALORIE_PER_KELVIN_MOLE,
-};
-
-const TEMPERATURE_25_C_KELVIN: f64 = 298.15;
-
 /// RT in kcal/mol at 25 °C, using the CODATA 2022 molar gas constant.
-pub(crate) const RT: f64 = MOLAR_GAS_CONSTANT.get_with_si_unit() * TEMPERATURE_25_C_KELVIN
-    / KILOCALORIE_PER_KELVIN_MOLE.get_with_si_unit();
+pub(crate) const RT: f64 = 0.5924849497028442;
 pub(crate) const A: f64 = 2. * (1. / 10.5 + 1. / 12.);
 pub(crate) const B: f64 = 0.4;
 pub(crate) const K_RT: f64 = -1100. / 4363.;
